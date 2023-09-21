07a4a1bfcb2d9058f26b7e2d7530121990c04e0a
ICE0208 <doky3307@naver.com>
2023-09-21 23:15:48 +0900

    코드 정리

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,19 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import Globalstyle from './GlobalStyle';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-const queryClient = new QueryClient();
 
 function Root() {
   return (
     <>
-      <Globalstyle />
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        <h1>Header</h1>
-        <Outlet />
-      </QueryClientProvider>
+      <h1>Header</h1>
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,25 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Globalstyle from './GlobalStyle.ts';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<RouterProvider router={router} />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <Globalstyle />
+    <ReactQueryDevtools />
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
+);
