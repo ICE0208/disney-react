e441914cc369dc0623abf0413819a543ae18bc73
ICE0208 <doky3307@naver.com>
2023-09-24 13:30:23 +0900

    ThemeProvider 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import router from './Router.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Globalstyle from './GlobalStyle.ts';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme.ts';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,9 +19,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <Globalstyle />
-    <ReactQueryDevtools />
-    <RouterProvider router={router} />
-  </QueryClientProvider>,
+  <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <Globalstyle />
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </ThemeProvider>,
 );
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+// my-theme.ts
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  mainTextColor: 'whitesmoke',
+  boxTextColor: 'black',
+  boxColor: 'whitesmoke',
+};
+
+export { theme };
